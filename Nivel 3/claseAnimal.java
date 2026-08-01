a5b8e64e345d9e5ce356b1b63910baeb5566b09d
Xavier Quina <rtxavicastro11@gmail.com>
2026-07-31 22:05:06 -0600

    Ejercicios de polimorfismo fueron agregados

--- Nivel 3/claseAnimal.java
+++ Nivel 3/claseAnimal.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 //Ejercicio 1: Crear clase Animal.
 class Animal{
     protected String nombre;
@@ -25,10 +27,40 @@ class Perro extends Animal{
     }
 }
 
+//Ejercicio 6: Crear clase Gato que herede de Animal.
+class Gato extends Animal{
+    public Gato(String nombre){
+        super(nombre);
+    }
+
+    //Ejercicio 7: Sobrescribir método sonido().
+    @Override
+    public void sonido(){
+        System.out.println(nombre+" esta maullando");
+    }
+}
+
 public class claseAnimal {
     public static void main(String[] args){
         //Ejercicio 5: Crear objeto y probar.
         Perro perro1 = new Perro("Benji");
         perro1.sonido();
+
+        Gato gato1 = new Gato("Rufus");
+        gato1.sonido();
+
+        //Ejercicio 8: Crear lista de animales.
+        ArrayList<Animal> animales = new ArrayList<>();
+
+        animales.add(new Perro("Bobby"));
+        animales.add(new Gato("Tammy"));
+        animales.add(new Perro("Toby"));
+        animales.add(new Gato("Milo"));
+
+        //Ejercicio 9: Recorrer lista y ejecutar métodos.
+        for(Animal a : animales){
+            //Ejercicio 10: Ejecutar un mismo método en distintos objetos.
+            a.sonido();
+        }
     }
 }
